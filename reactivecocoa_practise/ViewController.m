31ec2ab01a16e8fce74c99cc9248476871fa9833
臧成威 <zangchengwei@meituan.com>
2016-07-17 03:14:45 +0800

    第三周作业

--- reactivecocoa_practise/ViewController.m
+++ reactivecocoa_practise/ViewController.m
@@ -8,9 +8,10 @@
 
 #import "ViewController.h"
 #import <ReactiveCocoa.h>
+#import <Masonry.h>
 
 @interface ViewController ()
-@property (weak, nonatomic) IBOutlet UIImageView *spirit;
+
 @property (weak, nonatomic) IBOutlet UIView *grid;
 @property (weak, nonatomic) IBOutlet UIButton *autoRunBtn;
 @property (weak, nonatomic) IBOutlet UIButton *oneStepBtn;
@@ -20,6 +21,11 @@
 static int GridXBlocks = 13;
 static int GridYBlocks = 7;
 
+typedef NS_ENUM(NSUInteger, SpiritState) {
+    SpiritStateAppear,
+    SpiritStateRunning,
+    SpiritStateDisappear,
+};
 
 @implementation ViewController
 
@@ -29,32 +35,92 @@ static int GridYBlocks = 7;
     UIImage *img2 = [UIImage imageNamed:@"pet2"];
     UIImage *img3 = [UIImage imageNamed:@"pet3"];
     
-    self.spirit.animationImages = @[img1, img2, img3];
-    self.spirit.animationDuration = 1.0;
-    [self.spirit startAnimating];
-    
-    RACSignal *stepSignal = @[RACTuplePack(@1, @0), RACTuplePack(@1, @0),
-                              RACTuplePack(@1, @0), RACTuplePack(@0, @1),
-                              RACTuplePack(@0, @1), RACTuplePack(@0, @1),
-                              RACTuplePack(@1, @0), RACTuplePack(@1, @0),
-                              RACTuplePack(@1, @0), RACTuplePack(@1, @0),
-                              RACTuplePack(@0, @-1), RACTuplePack(@0, @-1),
-                              RACTuplePack(@1, @0), RACTuplePack(@1, @0),
-                              RACTuplePack(@1, @0)
-                              ].rac_sequence.signal;
+    NSArray *steps = @[RACTuplePack(@1, @0), RACTuplePack(@1, @0),
+                       RACTuplePack(@1, @0), RACTuplePack(@0, @1),
+                       RACTuplePack(@0, @1), RACTuplePack(@0, @1),
+                       RACTuplePack(@1, @0), RACTuplePack(@1, @0),
+                       RACTuplePack(@1, @0), RACTuplePack(@1, @0),
+                       RACTuplePack(@0, @-1), RACTuplePack(@0, @-1),
+                       RACTuplePack(@1, @0), RACTuplePack(@1, @0),
+                       RACTuplePack(@1, @0)
+                       ];
     
     RACTuple *startBlock = RACTuplePack(@1, @2);
     
+    RACSequence *stepsSequence = steps.rac_sequence;
+    
+    NSInteger spiritCount = steps.count + 1; // 步数 + 1个起始位置
+    
+    void (^updateXYConstraints)(UIView *view, RACTuple *location) = ^(UIView *view, RACTuple *location) {
+        CGFloat width = self.grid.frame.size.width / GridXBlocks;
+        CGFloat height = self.grid.frame.size.height / GridYBlocks;
+        CGFloat x = [location.first floatValue] * width;
+        CGFloat y = [location.second floatValue] * height;
+        view.frame = CGRectMake(x, y, width, height);
+    };
+    
+    for (int i = 0; i < spiritCount; ++i) {
+        UIImageView *spiritView = [[UIImageView alloc] init];
+        
+        spiritView.tag = i;
+        spiritView.animationImages = @[img1, img2, img3];
+        spiritView.animationDuration = 1.0;
+        spiritView.alpha = 0.0f;
+        [self.grid addSubview:spiritView];
+        
+        updateXYConstraints(spiritView, startBlock);
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     RACSignal *spiritRunSignal = nil;
     @weakify(self)
-    [spiritRunSignal subscribeNext:^(RACTuple *xy) {
+    [[spiritRunSignal deliverOnMainThread] subscribeNext:^(RACTuple *info) {
         @strongify(self)
-        RACTupleUnpack(NSNumber *x, NSNumber *y) = xy;
-        CGFloat spiritHeight = self.grid.frame.size.height / GridYBlocks;
-        CGFloat spiritWidth = self.grid.frame.size.width / GridXBlocks;
-        [UIView animateWithDuration:1 animations:^{
-            self.spirit.frame = CGRectMake(spiritWidth * x.integerValue, spiritHeight * y.integerValue, spiritWidth, spiritHeight);
-        }];
+        RACTupleUnpack(NSNumber *idx, NSNumber *state, RACTuple *xy) = info;
+        SpiritState stateValue = state.unsignedIntegerValue;
+        NSInteger idxValue = idx.integerValue;
+        UIImageView *spirit = [self.grid viewWithTag:idxValue];
+        
+        switch (stateValue) {
+            case SpiritStateAppear:
+            {
+                updateXYConstraints(spirit, xy);
+                [UIView animateWithDuration:1 animations:^{
+                    spirit.alpha = 1.0f;
+                }];
+                [spirit startAnimating];
+            }
+                break;
+            case SpiritStateRunning:
+            {
+                
+                [UIView animateWithDuration:1 animations:^{
+                    updateXYConstraints(spirit, xy);
+                }];
+            }
+                break;
+            case SpiritStateDisappear:
+            {
+                [UIView animateWithDuration:1 animations:^{
+                    spirit.alpha = 0.0f;
+                } completion:^(BOOL finished) {
+                    [spirit stopAnimating];
+                }];
+                
+            }
+                break;
+            default:
+                break;
+        }
+        
         
     }];
     
